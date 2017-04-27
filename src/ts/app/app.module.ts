3ae37bc2e36f8e0277586fdee7427a6424aa3e73
Eric W. Greene <eric@ericwgreene.com>
2017-04-26 20:32:32 -0400

    tweaked configuration

--- src/ts/app/app.module.ts
+++ src/ts/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 
+import "bootstrap-loader";
 import "../../scss/styles.scss";
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule, FormsModule, NgbModule.forRoot() ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ],
 })

1690c45254654af73ae5ef3ce516305bfbac3c28
Eric W. Greene <eric@ericwgreene.com>
2017-05-08 10:56:33 -0400

    added code demos

--- src/ts/app/color-tool/components/color-tool.component.ts
+++ src/ts/app/color-tool/components/color-tool.component.ts
@@ -1,11 +1,28 @@
 import { Component } from "@angular/core";
 
+import { Color } from "../models/color";
+
 @Component({
     selector: "color-tool",
     template: `
         <header>
             <h1>Color Tool</h1>
         </header>
+        <ul>
+            <li *ngFor="let color of colors">{{color.name}}</li>
+        </ul>
     `,
 })
-export class ColorToolComponent { }
+export class ColorToolComponent {
+
+    public colors: Color[] = [
+        { id: 1, name: "red" },
+        { id: 2, name: "gold" },
+        { id: 3, name: "green" },
+        { id: 4, name: "white" },
+        { id: 5, name: "saffron" },
+        { id: 6, name: "blue" },
+    ];
+
+
+}
